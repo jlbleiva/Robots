ce10562074796ca84711b1b15bcb4e9d6da63f57
JOSE LUIS BENITEZ <joseluis.benitez@dxc.com>
2021-10-11 17:04:48 +0200

    habia creado interface y clases derivadas

--- Robots/Program.cs
+++ Robots/Program.cs
@@ -1,47 +1,102 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel.Design;
 
 namespace Robots
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            var robots = new Collection<Robot>(){ new RobotRaspi() , new RobotPC(), new RobotArduino() };
+            var pc = new RobotPC(new RobotPCBehavior());
+            var arduino = new RobotArduino(new RobotArduinoBehavior());
+            var raspi = new RobotRaspi(new RobotRaspiBehavior());
+
+            var robots = new Collection<Robot>(){pc,arduino,raspi};
             
             //intocable
             foreach (var robot in robots)
             {
+               
                 robot.DoStuff();
             }
             
         }
     }
 
-    public abstract class Robot
+    
+    public interface IRobot
     {
-        public abstract void DoStuff();
+        public void Show();
     }
 
-    public class RobotRaspi:Robot
+    public abstract class Robot
     {
-        public override void DoStuff()
+        private IRobot _Robotbehavior;
+        
+        public Robot(IRobot behavior)
         {
-            Console.WriteLine("el texto de DoStuff para el de RobotRaspi");
+            _Robotbehavior = behavior;
         }
+
+        public void DoStuff()
+        {
+            _Robotbehavior.Show();
+        }
+
+        //cambia el comportamiento de cada tipo de robot
+        public void ChangeBehavior(IRobot robotBehavior)
+        {
+            _Robotbehavior = robotBehavior;
+        }
+    }
+    
+    public class RobotRaspi: Robot
+    {
+        public RobotRaspi(IRobot _robotBahavior) : base(_robotBahavior)
+        {
+
+        }
+        
     }
     public class RobotPC: Robot
     {
-        public override void DoStuff()
+        public RobotPC(IRobot _robotBahavior) : base(_robotBahavior)
         {
-            Console.WriteLine("el texto de DoStuff para el RobotPC");
+
         }
     }
     public class RobotArduino: Robot 
     {
-        public override void DoStuff()
+        public RobotArduino(IRobot _robotBehavior) : base(_robotBehavior)
         {
-            Console.WriteLine("el texto de DoStuff para el RobotArduino");
+
+        }
+        
+    }
+
+    public class RobotPCBehavior : IRobot
+    {
+       public void Show()
+        {
+          Console.WriteLine("Soy PC listo");
+        }
+    }
+    public class RobotArduinoBehavior : IRobot
+    {
+        
+        public void Show()
+        {
+            Console.WriteLine("Soy Arduino listo");
+        }
+    }
+    public class RobotRaspiBehavior : IRobot
+    {
+
+        public void Show()
+        {
+            Console.WriteLine("Soy Raspi listo");
         }
     }
 }
